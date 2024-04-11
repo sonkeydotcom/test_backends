33a95ddd41ed1aba13e08b556dd0ffaa570b2c43
Davidhero <owogogahhero@outlook.com>
2024-04-11 07:20:08 +0100

    add jwt && auth strategy

---
+++ src/auth/decorator/jwt-auth-guard.decorator.ts
@@ -0,0 +1,33 @@
+import { ExecutionContext } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from './is-public.decorator';
+import { Request } from 'express';
+
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+    private reflector: Reflector,
+  ) {
+    super();
+  }
+
+  async canActivate(context: ExecutionContext): Promise<any | boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context);
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === `Bearer` ? token : undefined;
+  }
+}
